fc9987a3b736f3d43fcc6ae8cb2d88b69550589b
teacherIan <ian_malloy@ssis.asia>
2022-02-21 15:15:34 +0800

    For G11 class.  Basic React. Fixed

---
+++ src/Person.js
@@ -0,0 +1,12 @@
+import './Person.css'
+
+function Person() {
+    return(
+        <div className='box'>
+            <div>Doug Malloy</div>
+            <div>70</div>
+        </div>
+    )
+}
+
+export default Person;
